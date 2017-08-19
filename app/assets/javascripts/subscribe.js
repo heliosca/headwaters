b332b1e0aa360e907f1f667dadfaea88f12255e3
alookatommorow <hessjohnt@gmail.com>
2017-08-18 18:23:45 -0700

    add mailchimp index

--- app/assets/javascripts/subscribe.js
+++ app/assets/javascripts/subscribe.js
@@ -1,9 +1,11 @@
 document.addEventListener("turbolinks:load", function() {
   var hwCookie = document.cookie.match(/^(.*;)?\s*HeadwatersCaCookie\s*=\s*[^;]+(.*)?$/);
 
-  setTimeout(function() {
-    $("#subscribe-window").fadeIn(400);
-  }, 1500);
+  if (!hwCookie) {
+    setTimeout(function() {
+      $("#subscribe-window").fadeIn(400);
+    }, 1500);
+  }
 
   $("#subscribe-window .close").click(function() {
     $("#subscribe-window").fadeOut();
@@ -15,10 +17,18 @@ document.addEventListener("turbolinks:load", function() {
 
     var url = $(this).attr('action'),
         data = $(this).serialize(),
-        success = function(response) {
-          console.log(response);
-        };
+        success = subscribeSuccess;
 
-    $.post(url, data, success, 'json');
+    $.post(url, data, success);
   });
+
+  function subscribeSuccess(response) {
+    console.log(response);
+    $("#subscribe-window").fadeOut();
+    setCookie();
+  }
+
+  function setCookie() {
+    document.cookie = "HeadwatersCaCookie=scrumptious";
+  }
 });
