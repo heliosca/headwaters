264235137e5c0e6956067635a4bbcf9ccc491032
alookatommorow <hessjohnt@gmail.com>
2017-08-18 18:31:10 -0700

    extract subscribe pop up to partial

--- app/assets/javascripts/subscribe.js
+++ app/assets/javascripts/subscribe.js
@@ -1,11 +1,11 @@
 document.addEventListener("turbolinks:load", function() {
   var hwCookie = document.cookie.match(/^(.*;)?\s*HeadwatersCaCookie\s*=\s*[^;]+(.*)?$/);
 
-  if (!hwCookie) {
+  // if (!hwCookie) {
     setTimeout(function() {
       $("#subscribe-window").fadeIn(400);
     }, 1500);
-  }
+  // }
 
   $("#subscribe-window .close").click(function() {
     $("#subscribe-window").fadeOut();
