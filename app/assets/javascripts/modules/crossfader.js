7a7ce6d2cd39bb05fd0148a05d4271e23434e556
alookatommorow <hessjohnt@gmail.com>
2017-08-23 15:04:02 -0700

    cache crossfader photo variables

--- app/assets/javascripts/modules/crossfader.js
+++ app/assets/javascripts/modules/crossfader.js
@@ -1,5 +1,5 @@
 function crossFader(){
-  var interval;
+  var interval, $next, $active;
 
   this.init = function() {
     interval = setInterval(crossFade, 3000);
@@ -10,8 +10,8 @@ function crossFader(){
   };
 
   function crossFade() {
-    var $active = $('.about-images .active'),
-        $next = ($active.next().length > 0) ? $active.next() : $('.about-images img:first');
+    $active = $('.about-images .active');
+    $next = ($active.next().length > 0) ? $active.next() : $('.about-images img:first');
     $next.css('z-index',2);//move the next image up the pile
     $active.fadeOut(1500, function() {//fade out the top image
       $active.css('z-index', 1).show().removeClass('active');//reset the z-index and unhide the image
