69ca7da4a256af2062b20fde68e683744d15030d
alookatommorow <hessjohnt@gmail.com>
2017-09-06 11:34:14 -0700

    add products controller/route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :subscriptions, only: [:create]
+  resources :products, only: [:index]
   root to: "welcome#index"
 
   match '/:id' => 'high_voltage/pages#show', as: :static, via: :get #remove '/pages' from url
