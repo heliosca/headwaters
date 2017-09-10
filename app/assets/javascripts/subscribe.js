ad4bfd37bb42ecf6c96d7f1398a1cbcf376681ac
alookatommorow <hessjohnt@gmail.com>
2017-09-10 12:27:58 -0700

    add footer content and styling

--- app/assets/javascripts/subscribe.js
+++ app/assets/javascripts/subscribe.js
@@ -1,42 +1,42 @@
-document.addEventListener("turbolinks:load", function() {
-  var hwCookie = document.cookie.match(/^(.*;)?\s*HeadwatersCaCookie\s*=\s*[^;]+(.*)?$/);
-  if (!hwCookie) {
-    if (!globals.optedOut) {
-      if (!globals.subscribeShown) {
-        setTimeout(function() {
-          $("#subscribe-window").fadeIn(400);
-          globals.subscribeShown = true;
-        }, 1500);
-      } else {
-        $("#subscribe-window").show();
-      }
-    }
+// document.addEventListener("turbolinks:load", function() {
+//   var hwCookie = document.cookie.match(/^(.*;)?\s*HeadwatersCaCookie\s*=\s*[^;]+(.*)?$/);
+//   if (!hwCookie) {
+//     if (!globals.optedOut) {
+//       if (!globals.subscribeShown) {
+//         setTimeout(function() {
+//           $("#subscribe-window").fadeIn(400);
+//           globals.subscribeShown = true;
+//         }, 1500);
+//       } else {
+//         $("#subscribe-window").show();
+//       }
+//     }
 
-    $("#subscribe-window .close").click(function() {
-      $("#subscribe-window").fadeOut(function() {
-        $("#subscribe-window").remove();
-      });
-      globals.optedOut = true;
-    });
+//     $("#subscribe-window .close").click(function() {
+//       $("#subscribe-window").fadeOut(function() {
+//         $("#subscribe-window").remove();
+//       });
+//       globals.optedOut = true;
+//     });
 
-    $("#subscribe-form").submit(function() {
-      event.preventDefault();
-      console.log($(this).attr('action'));
+//     $("#subscribe-form").submit(function() {
+//       event.preventDefault();
+//       console.log($(this).attr('action'));
 
-      var url = $(this).attr('action'),
-          data = $(this).serialize(),
-          success = subscribeSuccess;
+//       var url = $(this).attr('action'),
+//           data = $(this).serialize(),
+//           success = subscribeSuccess;
 
-      $.post(url, data, success);
-    });
-  }
+//       $.post(url, data, success);
+//     });
+//   }
 
-  function subscribeSuccess(response) {
-    $("#subscribe-window").fadeOut();
-    setCookie();
-  }
+//   function subscribeSuccess(response) {
+//     $("#subscribe-window").fadeOut();
+//     setCookie();
+//   }
 
-  function setCookie() {
-    document.cookie = "HeadwatersCaCookie=scumptious; expires="+globals.dateYearFromNow();
-  }
-});
+//   function setCookie() {
+//     document.cookie = "HeadwatersCaCookie=scumptious; expires="+globals.dateYearFromNow();
+//   }
+// });
