a45c2f0fb91fe42b4b482ab7efbfad9265d23a12
alookatommorow <hessjohnt@gmail.com>
2017-10-02 19:38:37 -0700

    add benefits to products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170928170056) do
+ActiveRecord::Schema.define(version: 20171003022912) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20170928170056) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
     t.string   "weed_type"
+    t.text     "benefits"
   end
 
 end
