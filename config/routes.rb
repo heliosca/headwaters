2fa33089fbcc0ae84734d095e158c41800394944
alookatommorow <hessjohnt@gmail.com>
2017-10-03 00:25:21 -0700

    add prod show

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :subscriptions, only: [:create]
-  resources :products, only: [:index]
+  resources :products, only: [:index, :show]
   root to: "welcome#index"
 
   match '/:id' => 'high_voltage/pages#show', as: :static, via: :get #remove '/pages' from url
