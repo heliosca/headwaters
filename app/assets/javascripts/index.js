acc684e39d4b3c1f98e363ac656638e532aa7d60
alookatommorow <hessjohnt@gmail.com>
2017-10-05 19:50:45 -0700

    fix carousel bug

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,14 +1,14 @@
 document.addEventListener("turbolinks:load", function() {
+  if (globals.crossFaderRunning) {
+    globals.HWcrossFader.stop();
+    globals.crossFaderRunning = false;
+  }
+
   if (window.location.pathname === "/") {
     if (!globals.HWcrossFader) {
       globals.HWcrossFader = new crossFader();
     }
     globals.HWcrossFader.init();
     globals.crossFaderRunning = true;
-  } else {
-    if (globals.crossFaderRunning) {
-      globals.HWcrossFader.stop();
-      globals.crossFaderRunning = false;
-    }
   }
 });
