b566e0c69eb6ba76942c1284a164c330364926c5
alookatommorow <hessjohnt@gmail.com>
2017-10-17 10:04:24 -0700

    update prod views/styling

--- app/helpers/image_url_helper.rb
+++ app/helpers/image_url_helper.rb
@@ -2,4 +2,8 @@ module ImageUrlHelper
   def hw_image_url(image_name)
 		"https://s3-us-west-1.amazonaws.com/headwatersca/images/" << image_name	
   end
+
+  def hw_product_image_url(image_name)
+		"https://s3-us-west-1.amazonaws.com/headwatersca/images/products/" << image_name	
+  end
 end
